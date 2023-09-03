fe29ad2f30532fceae61da9b688682bad1acce17
Claude Mendelson <claudenm@users.noreply.github.com>
2023-09-03 10:48:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from logging import getLogger
+log = getLogger(__name__)
 
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
@@ -9,4 +11,5 @@ celsius = st.number_input("Enter temperature in Celsius:", value=0.0)
 
 fahrenheit = celsius_to_fahrenheit(celsius)
 print("test logging")
+log.info("test logger")
 st.write(f"{celsius:.2f} degrees Celsius is equal to {fahrenheit:.2f} degrees Fahrenheit.")
