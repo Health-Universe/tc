749ad0a6c4d23599d5eae152733ec7cfef3bb6ce
Claude Mendelson <claudenm@users.noreply.github.com>
2023-09-03 10:39:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ st.title("Celsius to Fahrenheit Converter")
 celsius = st.number_input("Enter temperature in Celsius:", value=0.0)
 
 fahrenheit = celsius_to_fahrenheit(celsius)
-
+print("test logging")
 st.write(f"{celsius:.2f} degrees Celsius is equal to {fahrenheit:.2f} degrees Fahrenheit.")
